effb7c7302d4fee593574b8c6489492409ac7e05
Manuel <mt.yactayo@gmail.com>
2023-08-11 22:35:33 -0500

    metodod para generar el numero de la orden

--- src/main/java/com/ventas/ecomerce/service/OrdenService.java
+++ src/main/java/com/ventas/ecomerce/service/OrdenService.java
@@ -1,7 +1,14 @@
 package com.ventas.ecomerce.service;
 
 import com.ventas.ecomerce.model.Orden;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 public interface OrdenService {
+
+    List<Orden> findAll();
     Orden save(Orden orden);
+
 }
