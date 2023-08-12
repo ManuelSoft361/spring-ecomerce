a1b92891eb8799a55b1f8e1af619df1dbf65ef93
Manuel <mt.yactayo@gmail.com>
2023-08-12 11:57:27 -0500

    funcionalidad para buscar un producto

--- src/main/java/com/ventas/ecomerce/controller/HomeController.java
+++ src/main/java/com/ventas/ecomerce/controller/HomeController.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 @CrossOrigin(origins = "http://localhost")
@@ -149,5 +150,13 @@ public class HomeController {
         detalles.clear();
 
         return "redirect:/";
+    }
+
+    @PostMapping("/search")
+    public String searchProducto(@RequestParam String nombre, Model model){
+        System.out.println("Nombre del producto: " + nombre);
+        List<Producto> productos=productoService.findAll().stream().filter(p->p.getNombre().contains(nombre)).collect(Collectors.toList());
+        model.addAttribute("productos",productos);
+        return "usuario/home";
     }
 }
